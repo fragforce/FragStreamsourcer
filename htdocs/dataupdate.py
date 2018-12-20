4a1652a06a6c20af2b8f92107e3b4aae517b6a68
Benjamin Gray <bgray@salesforce.com>
2018-12-20 00:09:32 -0800

    Dev Ramp Up
    Cleaning up initial deploys because they were terrible.  Renamed and moved things around, removed superflous code, and added real comments

--- htdocs/dataupdate.py
+++ htdocs/dataupdate.py
@@ -1,3 +1,12 @@
+########
+# dataupdate.py - Python script for updating data to local server from EL's API
+# 	this script should be running continuously in the background during streaming,
+#	as it updates local json records from Extra Life's API.  
+#	@dev - Ben Gray
+########
+
+#	TODO -- graceful exit for script after ctrl-c, rather than slamming it and waiting till the next update
+
 import urllib.request
 import threading
 
@@ -11,19 +20,25 @@ personID = '298779'
 pingSeconds = 60.0
 
 def getdata():
+
+	# Start thread for next call first
 	threading.Timer(pingSeconds, getdata).start()
+
+	# Ping api of whole team's donations and write to teamdonations.json
 	response = urllib.request.urlopen('https://www.extra-life.org/api/teams/' + teamID + '/donations')
 	html = response.read()
 	fout = open("teamdonations.json","w")
 	fout.write(html.decode("utf-8"))
 	fout.close()
 	
+	# Ping api of individual user and write to persondonations.json
 	response2 = urllib.request.urlopen('https://www.extra-life.org/api/participants/' + personID + '/donations')
 	html2 = response2.read()
 	fout2 = open("persondonations.json","w")
 	fout2.write(html2.decode("utf-8"))
 	fout2.close()
 	
-	print("server PING")
+	# Write teminal out line
+	print("Data Updated")
 	
 getdata()
